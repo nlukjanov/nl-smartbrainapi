9678bfb1b362a5edc01d1a7239a077f7d636eeab
Nikita Lukjanov <nlukjanov@gmail.com>
2019-05-29 17:59:19 +0300

    added profile get

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'nikitalukjanov',
-    password : '',
-    database : 'smart-brain'
+    host: '127.0.0.1',
+    user: 'nikitalukjanov',
+    password: '',
+    database: 'smart-brain'
   }
 });
 
-db.select('*').from('users').then(data => {
-  console.log('data', data);
-})
+db.select('*')
+  .from('users')
+  .then(data => {
+    console.log('data', data);
+  });
 const app = express();
 
 const database = {
@@ -76,24 +78,26 @@ app.post('/register', (req, res) => {
       email: email,
       name: name,
       joined: new Date()
-    }).then(user => {
+    })
+    .then(user => {
       res.json(user[0]);
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .catch(err => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(404).json('user not found');
-  }
+  db.select('*')
+    .from('users')
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('not found');
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'));
 });
 
 app.put('/image', (req, res) => {
