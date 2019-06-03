a806ed87de11e65a224d832080c7c8c0c45253d5
Nikita Lukjanov <nlukjanov@gmail.com>
2019-06-03 19:05:15 +0300

    moved clarifai API from front to end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '96dcaa29d916451eb8012d5a20c4564d'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -8,8 +23,9 @@ const handleImage = (req, res, db) => {
       res.json(entries[0]);
     })
     .catch(err => res.status(400).json('unable to get entries'));
-}
+};
 
 module.exports = {
-  handleImage
-}
+  handleImage,
+  handleApiCall
+};
