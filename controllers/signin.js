cd855106f91db92e90aa69a56a19557d4e54b232
Nikita Lukjanov <nlukjanov@gmail.com>
2019-06-03 19:05:03 +0300

    added user validations

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,14 +1,19 @@
 const handleSignin = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+
   db.select('email', 'hash')
     .from('login')
-    .where('email', '=', req.body.email)
+    .where('email', '=', email)
     .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db
           .select('*')
           .from('users')
-          .where('email', '=', req.body.email)
+          .where('email', '=', email)
           .then(user => {
             console.log('user', user);
             res.json(user[0]);
@@ -19,8 +24,8 @@ const handleSignin = (req, res, db, bcrypt) => {
       }
     })
     .catch(err => res.status(400).json('wrong credentials'));
-}
+};
 
 module.exports = {
   handleSignin
-}
+};
